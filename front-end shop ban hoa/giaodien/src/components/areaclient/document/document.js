d197cb242efa8a376a4c000a04cb7bdbacc5654a
duy99uit <58467999+duy99uit@users.noreply.github.com>
2021-01-06 16:54:42 +0700

    document layout fixed part2

--- front-end shop ban hoa/giaodien/src/components/areaclient/document/document.js
+++ front-end shop ban hoa/giaodien/src/components/areaclient/document/document.js
@@ -64,28 +64,28 @@ class document extends Component {
                 <div className="shop-detail-box-main">
                     <div className="container">
                         <div className="row" >
-                            <h1 style={{ fontWeight: "bold" }}>{this.state.data.name}</h1>
+                            <h2 style={{ fontWeight: "bold" }}>{this.state.data.name}</h2>
                         </div>
 
-                        <div className="row">
+                        <div className="row" style={{marginBottom: '20px'}}>
                             <div className="col-lg-6">
-                                <div className="banner-frame"> 
-                                <img className="img-fluid" src="images/about-img-1.jpg" alt="" />
+                                <div className="banner-frame">
+                                    <img className="img-fluid" src="images/about-img-1.jpg" alt="" />
                                 </div>
                             </div>
                             <div className="col-lg-6">
-                                <h2 className="noo-sh-title-top">Giới thiệu về <span>Freshshop</span></h2>
+                                <h4 className="noo-sh-title-top">Giới thiệu về <span className="document-brand">Freshshop</span></h4>
                                 <p className="about-content">Flower Shop là website phân phối sản phẩm về các loại hoa diệp lễ, hóa cưới, … hàng đầu Việt Nam.
 
 Flower Shop được sinh ra với mong muốn mang cung cấp cho người Việt Nam những bó hoa chất lượng nhất trên thế giới với một phương thức thanh toán đơn giản nhất, giá tốt nhất cùng với đó là dịch vụ chăm sóc khách hàng tuyệt vời. Chúng tôi mong muốn trở thành những người đồng hành và tư vấn cho bạn trong mọi vấn đề liên quan đến Hoa, giúp cho cuộc sống của mọi người giàu màu sắc và mơ mộng. Chúng tôi tin rằng giải trí đúng cách sẽ mang lại những giá trị tốt đẹp cho cuộc sống.</p>
-                               
+
                                 {/* <a className="btn hvr-hover" href="/#">Read More</a> */}
                             </div>
                         </div>
-                        
+ <hr />
 
                         <div className="row my-5">
-                            <div className="document-content" style={{fontSize:'20px'}}
+                            <div className="document-content" style={{ fontSize: '20px' }}
                                 dangerouslySetInnerHTML={{
                                     __html: this.state.data.content
                                 }}>
